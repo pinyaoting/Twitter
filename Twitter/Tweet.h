068d160f62fd3c2310f0eb99ec02ce0963038bd8
pinyaoting <pinyaoting@gmail.com>
2015-02-04 23:05:31 -0800

    commit missing files

---
+++ Twitter/Tweet.h
@@ -0,0 +1,21 @@
+//
+//  Tweet.h
+//  Twitter
+//
+//  Created by Pythis Ting on 2/4/15.
+//  Copyright (c) 2015 Yahoo!, inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "User.h"
+
+@interface Tweet : NSObject
+
+@property (nonatomic, strong) NSString *text;
+@property (nonatomic, strong) NSDate *createdAt;
+@property (nonatomic, strong) User *user;
+
+- (id)initWithDictionary:(NSDictionary *)dictionary;
++ (NSArray *)tweetsWithArray:(NSArray *)array;
+
+@end
---
+++ Twitter/Tweet.m
@@ -0,0 +1,39 @@
+//
+//  Tweet.m
+//  Twitter
+//
+//  Created by Pythis Ting on 2/4/15.
+//  Copyright (c) 2015 Yahoo!, inc. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+- (id)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    
+    if (self) {
+        self.user = [[User alloc] initWithDictionary:dictionary[@"user"]];
+        self.text = dictionary[@"text"];
+        NSString *createdAtString = dictionary[@"created_at"];
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"EEE MMM d HH:mm:ss Z y";
+
+        self.createdAt = [formatter dateFromString:createdAtString];
+    }
+    
+    return self;
+}
+
++ (NSArray *)tweetsWithArray:(NSArray *)array {
+    NSMutableArray *tweets = [NSMutableArray array];
+    
+    for (NSDictionary* dictionary in array) {
+        [tweets addObject:[[Tweet alloc] initWithDictionary:dictionary]];
+    }
+    
+    return tweets;
+}
+
+@end
