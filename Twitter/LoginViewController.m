ab04418eae38466521e524aa289f2f1a3af918f3
pinyaoting <pinyaoting@gmail.com>
2015-02-04 22:13:26 -0800

    added deep link

--- Twitter/LoginViewController.m
+++ Twitter/LoginViewController.m
@@ -29,6 +29,10 @@
     [[TwitterClient sharedInstance].requestSerializer removeAccessToken];
     [[TwitterClient sharedInstance] fetchRequestTokenWithPath:@"oauth/request_token" method:@"GET" callbackURL:[NSURL URLWithString:@"cptwitterdemo://oauth"] scope:nil success:^(BDBOAuth1Credential *requestToken) {
         NSLog(@"got the request token!");
+        
+        NSURL *authURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://api.twitter.com/oauth/authorize?oauth_token=%@", requestToken.token]];
+        [[UIApplication sharedApplication] openURL:authURL];
+        
     } failure:^(NSError *error) {
         NSLog(@"Failed to get the request token!");
     }];
